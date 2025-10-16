6932eade3564ea79e5fe221f480589358c01b0db
emilyclam <emilychilam@gmail.com>
2025-10-16 16:18:56 -0700

    fixed db schema and inserted items so all the data is in a json blob

--- backend/db/init/01_schema.sql
+++ backend/db/init/01_schema.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS bathrooms CASCADE;
+
 CREATE TABLE bathrooms (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  name TEXT NOT NULL,
-  position jsonb NOT NULL,
-  details TEXT
+  data JSONB NOT NULL
 );
--- backend/db/init/02_data.sql
+++ backend/db/init/02_data.sql
@@ -1,22 +1,28 @@
 DELETE FROM bathrooms;
-INSERT INTO bathrooms VALUES (
+
+INSERT INTO bathrooms (id, data) VALUES (
   '5f1169fe-4db2-48a2-b059-f05cfe63588b',
-  'Namaste Lounge Bathroom',
-  '{"lat": 37.00076576303953, "lng": -122.05719563060227}',
-  'more details'
+  '{
+    "name": "Namaste Lounge Bathroom",
+    "position": {"lat": 37.00076576303953, "lng": -122.05719563060227},
+    "details": "more details"
+  }'::jsonb
 );
 
-INSERT INTO bathrooms VALUES (
+INSERT INTO bathrooms (id, data) VALUES (
   'd924edd4-8291-4706-b472-9875319e8c18',
-  'E2 Second Floor Bathroom',
-  '{"lat": 37.00089593913641, "lng": -122.06296383018844}',
-  'more details'
+  '{
+    "name": "E2 Second Floor Bathroom",
+    "position": {"lat": 37.00089593913641, "lng": -122.06296383018844},
+    "details": "more details"
+  }'::jsonb
 );
 
-INSERT INTO bathrooms VALUES (
+INSERT INTO bathrooms (id, data) VALUES (
   '6d6a6a5f-217d-4fea-9ab4-1f21ea2c1b0a',
-  'McDonald`s Ocean St Bathroom',
-  '{"lat": 36.967136756404564, "lng": -122.03905636700443}',
-  'more details'
+  '{
+    "name": "McDonald`s Ocean St Bathroom",
+    "position": {"lat": 36.967136756404564, "lng": -122.03905636700443},
+    "details": "more details"
+  }'::jsonb
 );
-
