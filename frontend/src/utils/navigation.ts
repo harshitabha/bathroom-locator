eec33ff4191bb071979689251d20fa8446c33481
Cheryl Chuang <chjchuan@ucsc.edu>
2025-10-20 14:56:52 -0700

    manually testing on mobile app

--- frontend/src/utils/navigation.ts
+++ frontend/src/utils/navigation.ts
@@ -1,4 +1,5 @@
 // opens Google Maps with walking directions to the specified latitude and longitude
+// automatically opens the app if on mobile or new tab with maps on desktop or if app not installed
 export function openWalkingDirections(lat: number, lng: number) {
   const url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}&travelmode=walking`;
   window.open(url, '_blank');
