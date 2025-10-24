9065c187c9c879c06bf67c6217107e9c97b709c0
emilyclam <emilychilam@gmail.com>
2025-10-23 17:02:40 -0700

    changed database setup so I can implement testing easier

--- backend/db/init/01_create_schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS bathrooms CASCADE;
-CREATE TABLE bathrooms (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  name TEXT NOT NULL,
-  position jsonb NOT NULL,
-  details TEXT
-);
