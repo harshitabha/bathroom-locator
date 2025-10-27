7ee0039484fbc7a1b8f794fb9dcc0266e0b6cade
Austin Nguyen <autanguy@ucsc.edu>
2025-10-27 03:15:14 -0700

    Fetch and display pins within range
    Added test, removed debugging
    
    merge

--- backend/src/db.js
+++ backend/src/db.js
@@ -33,3 +33,43 @@ export async function getBathrooms() {
     throw error;
   }
 }
+
+/**
+ * returns list of bathrooms in database within bounds
+ * @param {number} minLng minimum longitute of the bounds
+ * @param {number} minLat minimum latitute of the bounds
+ * @param {number} maxLng max longitute of the bounds
+ * @param {number} maxLat max latitute of the bounds
+ * @param {number} limit limit of bathrooms to fetch
+ * @returns {object} array of bathroom objects
+ */
+export async function getBathroomsInBounds(
+    minLng, minLat, maxLng, maxLat, limit = 200,
+) {
+  try {
+    const {rows} = await pool.query({
+      text: `
+        SELECT
+          b.id,
+          b.data->>'name' AS name,
+          b.data->>'details' AS details,
+          b.data->>'position' AS position
+        FROM bathrooms b
+        WHERE
+          ((b.data->'position'->>'lng')::double precision BETWEEN $1 AND $3)
+          AND
+          ((b.data->'position'->>'lat')::double precision BETWEEN $2 AND $4)
+        LIMIT $5
+      `,
+      values: [minLng, minLat, maxLng, maxLat, limit],
+    });
+
+    rows.forEach((bathroom) => {
+      bathroom.position = JSON.parse(bathroom.position);
+    });
+    return rows;
+  } catch (error) {
+    console.error('Database query error:', error);
+    throw error;
+  }
+}
