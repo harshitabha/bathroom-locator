a058c6c7cdf36040bcafc2abcc97a95c9cd485a3
ravikavya <43942009+ravikavya@users.noreply.github.com>
2025-11-13 13:13:18 -0800

    Get bathroom verification endpoint (#30)
    * get request for getting user's liked bathroomIds

--- backend/src/index.js
+++ backend/src/index.js
@@ -45,6 +45,8 @@ app.post('/bathroom', bathroom.createBathroom);
 
 app.get('/bathroom/updates', bathroom.getUpdates);
 
+app.get('/user/likes', bathroom.getUserLikes);
+
 app.post('/user/likes', bathroom.likeBathroom);
 
 app.delete('/user/likes', bathroom.unlikeBathroom);
